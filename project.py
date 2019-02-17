1c94fc92b75bd37db5a5e05bdee6040c13b26249
Тансылу Ахметова <45488234+tansylu@users.noreply.github.com>
2019-02-17 21:23:27 +0300

    Update project.py

--- project.py
+++ project.py
@@ -18,7 +18,26 @@ for i in range(len(BOYS)):
 heart = pygame.image.load(os.path.join('data', 'heart.png'))
 # загрузка необходимых изображений
     
-    
+class Particle(pygame.sprite.Sprite):
+    fire = [heart]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+ 
+    def __init__(self, pos, dx, dy):
+        super().__init__(all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+        self.velocity = [dx, dy]
+        self.rect.x, self.rect.y = pos
+        self.gravity = 1
+ 
+    def update(self):
+        self.velocity[1] += self.gravity
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        if not self.rect.colliderect([0,0,1000,1000]):
+            self.kill()
+        # класс частиц
 class Boy(pygame.sprite.Sprite):
     def __init__(self, x, surf, group):
         pygame.sprite.Sprite.__init__(self)
@@ -38,10 +57,32 @@ class Boy(pygame.sprite.Sprite):
             if b.rect.collidepoint(pos):
                 b.image = heart
         # замена изображения парня изображением сердца при нажатии
-        
-boys = pygame.sprite.Group() # создание группы
+class Bomb(pygame.sprite.Sprite):
+    def __init__(self, x, surf, group):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = surf
+        self.rect = self.image.get_rect(center=(x, 0))
+        self.add(group)  
+        self.speed = randint(5, 8)  
+ 
+    def update(self):
+        if self.rect.y < 1000:
+            self.rect.y += self.speed
+        else:
+            self.kill()
+    def create_particles(position):
+        particle_count = 20
+        numbers = range(-15, 15)
+        for _ in range(particle_count):
+            Particle(position, random.choice(numbers), random.choice(numbers))
+            # класс бомб
 
-Boy(randint(1, 900), BOYS_SURF[randint(0, 2)], boys) # первая картинка
+boys = pygame.sprite.Group() # создание группы
+bombs = pygame.sprite.Group()
+all_sprites = pygame.sprite.Group()
+Bomb(randint(50, 900), bomb, bombs)# первая картинка
+count = 1
+
 
 while 1: # игровой цикл
     for event in pygame.event.get():
@@ -49,7 +90,11 @@ while 1: # игровой цикл
             exit()
             # завершение
         elif event.type == pygame.USEREVENT:
-            Boy(randint(1, 900), BOYS_SURF[randint(0, 2)], boys)
+            if count%5 != 0:
+                Boy(randint(50, 900), BOYS_SURF[randint(0, 2)], boys)
+            else:
+                Bomb(randint(50, 900), bomb, bombs)
+            count+=1
             # появление спрайтов
         elif event.type == pygame.MOUSEBUTTONDOWN:
             pos = pygame.mouse.get_pos()
@@ -58,7 +103,11 @@ while 1: # игровой цикл
             # нажатие мышью
     
     screen.blit(image, [0, 0]) 
-    boys.draw(screen) # отрисовка спрайтов
-    pygame.display.update()# обновление поля
-    pygame.time.delay(10)
-    boys.update() # обновление группы
+   bombs.draw(screen)
+   boys.draw(screen)
+   all_sprites.draw(screen)# отрисовка спрайтов
+   pygame.display.update()# обновление поля
+   pygame.time.delay(10)
+   bombs.draw(screen)
+   boys.draw(screen)
+   all_sprites.draw(screen) # обновление групп
