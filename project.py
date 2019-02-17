5a7988165926750389899a0718fc9af2c403fc81
Тансылу Ахметова <45488234+tansylu@users.noreply.github.com>
2019-02-17 23:14:34 +0300

    Update project.py

--- project.py
+++ project.py
@@ -11,7 +11,9 @@ screen.blit(image, [0, 0])
 boom = pygame.mixer.Sound('boom.wav')
 sound = pygame.mixer.Sound('action.wav') # добавление звука при нажатии
 # инициализация поля 
-    
+n, h, j, c, d = 0, 5, 8, 8, 15
+# очки и скорость
+
 BOYS = ('boy.jpg','boy1.jpg', 'boy2.jpg', 'boy3.jpg')
 BOYS_SURF = []  
 for i in range(len(BOYS)):
@@ -53,17 +55,6 @@ class Boy(pygame.sprite.Sprite):
         else:
             self.kill()
         # функция, удаляющая спрайт за пределами поля
-   def love(boys, pos):
-        for b in boys:
-            if b.rect.collidepoint(pos):
-                sound.play()
-                b.image = heart
-        for b in bombs:
-            if b.rect.collidepoint(pos):
-                boom.play()
-                b.kill()
-                Bomb.create_particles(pos)
-        # замена изображения парня изображением сердца при нажатии
 class Bomb(pygame.sprite.Sprite):
     def __init__(self, x, surf, group):
         pygame.sprite.Sprite.__init__(self)
@@ -77,11 +68,6 @@ class Bomb(pygame.sprite.Sprite):
             self.rect.y += self.speed
         else:
             self.kill()
-    def create_particles(position):
-        particle_count = 20
-        numbers = range(-15, 15)
-        for _ in range(particle_count):
-            Particle(position, random.choice(numbers), random.choice(numbers))
             # класс бомб
 
 boys = pygame.sprite.Group() # создание группы
@@ -89,7 +75,39 @@ bombs = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 Bomb(randint(50, 900), bomb, bombs)# первая картинка
 count = 1
-
+def create_particles(position):
+        particle_count = 20
+        numbers = range(-15, 15)
+        for _ in range(particle_count):
+            Particle(position, random.choice(numbers), random.choice(numbers))
+            # создание частиц 
+def love(boys, pos):
+        global n, h, j, c, d
+        for b in boys:
+            if b.rect.collidepoint(pos):
+                sound.play()
+                b.image = heart
+                n += 1
+                h += 2
+                j += 2
+        for b in bombs:
+            if b.rect.collidepoint(pos):
+                boom.play()
+                b.kill()
+                n += 10
+                c += 2
+                d += 2
+                create_particles(pos)
+        # замена изображения парня изображением сердца при нажатии
+def score(n):
+    font = pygame.font.SysFont( 'couriernew', 25)
+    text = font.render("Caught: "+str(n), True, (0, 0, 0))
+    sp = font.render("Boys_Speed: "+str(h)+'-'+str(j), True, (0, 0, 0))
+    spb = font.render("Bombs_Speed: "+str(c)+'-'+str(d), True, (0, 0, 0))
+    screen.blit(text,(0,0))
+    screen.blit(sp,(0,25))
+    screen.blit(spb,(0,50))
+    # вывод очков и скорости
 
 while 1: # игровой цикл
     for event in pygame.event.get():
@@ -108,7 +126,8 @@ while 1: # игровой цикл
             Boy.love(boys, pos)
             # нажатие мышью
     
-    screen.blit(image, [0, 0]) 
+   screen.blit(image, [0, 0]) 
+   score(n)
    bombs.draw(screen)
    boys.draw(screen)
    all_sprites.draw(screen)# отрисовка спрайтов
